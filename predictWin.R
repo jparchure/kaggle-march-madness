6cad124b4224659f2b30e9f40a413ef9bb951e7a
Innovation01 <innovation01@C02PJ26XG3QC.local>
2016-02-16 11:57:40 -0600

    More Scripts

--- predictWin.R
+++ predictWin.R
@@ -8,9 +8,11 @@ alltables <- dbListTables(conn)
 winnTeamsQuery<- "select TourneyCompactResults.*, Teams.Team_Name from TourneyCompactResults inner join Teams on Teams.Team_id = TourneyCompactResults.Wteam where TourneyCompactResults.Daynum=154"
 tourneySeedsQuery <- "select TourneySeeds.*, Teams.Team_Name from Teams inner join TourneySeeds on  Teams.team_id = TourneySeeds.Team order by TourneySeeds.Season, TourneySeeds.Seed, TourneySeeds.Team"
 seasonResultsQuery <- "select * from RegularSeasonCompactResults"
+teamListQuery <- "select * from Teams"
 tourneyWinners<- dbGetQuery(conn,winnTeamsQuery)
 tourneySeeds <- dbGetQuery(conn,tourneySeedsQuery)
 seasonResults<- dbGetQuery(conn,seasonResultsQuery)
+teamList<- dbGetQuery(conn,teamListQuery)
 extractSeedNums<- function(seedsTable, colname){
   #Function to help separate Seed numbers and Seed Zone
   regexpnum <- "[[:digit:]]+"
@@ -32,3 +34,17 @@ seasonwinsquery <- "select Wteam, Season, COUNT(*) from RegularSeasonCompactResu
 noofwins <- dbGetQuery(conn, seasonwinsquery)
 noOfWinsChampions <- merge(noofwins, tourneyWinners, by=c("Wteam","Season")) #No. of Wins in regular season by the eventual champion
 noOfWinsChampions["Differential"] <- noOfWinsChampions["Wscore"]- noOfWinsChampions["Lscore"]
+teamDifferential <- function(){
+  df = seasonResults
+  df["Daynum"] = NULL
+  df["Diff"] = seasonResults$Wscore - seasonResults$Lscore
+  #aggregate(df$Diff,by=list(df$Season, df$WTeam), FUN=sum)
+  #tapply(df$Diff, list(df$Season, df$WTeam), FUN=sum)
+
+  DT <- data.table(df)
+  return(data.frame(DT[, Diff:=sum(Diff), by=list(Season, Wteam)]))
+}
+
+head(teamDifferential())
+
+
