91b2845db974d554e6c94e96af93214d30cf1ec4
Innovation01 <innovation01@C02PJ26XG3QC.local>
2016-02-16 10:43:34 -0600

    More Queries

--- predictWin.R
+++ predictWin.R
@@ -7,8 +7,10 @@ conn <- dbConnect(sqlite,"database.sqlite")
 alltables <- dbListTables(conn)
 winnTeamsQuery<- "select TourneyCompactResults.*, Teams.Team_Name from TourneyCompactResults inner join Teams on Teams.Team_id = TourneyCompactResults.Wteam where TourneyCompactResults.Daynum=154"
 tourneySeedsQuery <- "select TourneySeeds.*, Teams.Team_Name from Teams inner join TourneySeeds on  Teams.team_id = TourneySeeds.Team order by TourneySeeds.Season, TourneySeeds.Seed, TourneySeeds.Team"
+seasonResultsQuery <- "select * from RegularSeasonCompactResults"
 tourneyWinners<- dbGetQuery(conn,winnTeamsQuery)
 tourneySeeds <- dbGetQuery(conn,tourneySeedsQuery)
+seasonResults<- dbGetQuery(conn,seasonResultsQuery)
 extractSeedNums<- function(seedsTable, colname){
   #Function to help separate Seed numbers and Seed Zone
   regexpnum <- "[[:digit:]]+"
@@ -25,6 +27,8 @@ extractSeedNums<- function(seedsTable, colname){
 tourneySeeds<- extractSeedNums(tourneySeeds, "Seed")
 head(tourneySeeds)
 
-winningSeeds <- merge(tourneyWinners, tourneySeeds,by.x = c("Season", "Wteam"), by.y=c("Season", "Team"))
-seasonwinsquery <- "select Wteam, COUNT(*) from RegularSeasonCompactResults GROUP BY Wteam"
+winningSeeds <- merge(tourneyWinners, tourneySeeds,by.x = c("Season", "Wteam"), by.y=c("Season", "Team")) #Get the Seeds of Tournament Winners
+seasonwinsquery <- "select Wteam, Season, COUNT(*) from RegularSeasonCompactResults  GROUP BY Wteam, Season"
 noofwins <- dbGetQuery(conn, seasonwinsquery)
+noOfWinsChampions <- merge(noofwins, tourneyWinners, by=c("Wteam","Season")) #No. of Wins in regular season by the eventual champion
+noOfWinsChampions["Differential"] <- noOfWinsChampions["Wscore"]- noOfWinsChampions["Lscore"]
